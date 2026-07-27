f330919e94bd2312c3abf53295bd110501519152
lhashing <lhashingj@gmail.com>
2026-07-27 19:13:35 +0600

    Finalize Sentry setup via official wizard
    Wizard converted config files to TypeScript, bumped @sentry/nextjs to
    v10.68.0, wired org/project into next.config.mjs, and added the
    example error-test page/route for verifying the integration.

---
+++ sentry.server.config.ts
@@ -0,0 +1,22 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://d064757a8dadc1d91e5aa902fcc5498a@o4511807237718016.ingest.us.sentry.io/4511807248924672",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
